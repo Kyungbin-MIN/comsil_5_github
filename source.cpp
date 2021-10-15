a10c33bd6f2c61e2de33da4a8c2769ababdecfa1
Kyungbin-MIN <kevin9859@sogang.ac.kr>
2021-10-15 19:20:02 +0900

    master

---
+++ source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char mode;
+	cout << "input mode : " << endl;
+	cin >> mode;
+	if (mode == m)
+		cout << "master" << endl;
+	return 0;
+}
