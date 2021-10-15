d2b6375b487156cc75ff4a6212becdd005f8db9b
Kyungbin-MIN <kevin9859@sogang.ac.kr>
2021-10-15 19:54:13 +0900

    third

--- source.cpp
+++ source.cpp
@@ -7,5 +7,13 @@ int main() {
 	cin >> mode;
 	if (mode == 'm')
 		cout << "master" << endl;
+	else if (mode == 'f')
+		cout << "frist" << endl;
+	else if (mode = 's')
+		cout << "second" << endl;
+	else if (mode = 't')
+		cout << "third" << endl;
+	else
+		cout << "ERROR!!" << endl;
 	return 0;
 }
